f895896bad798b15fd580e4e04a0bad6312c4369
ozunacristian <ozunacristianj@gmail.com>
2024-06-22 06:31:29 -0300

    Se añade datePicker, cambios en theme

---
+++ crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/DatePickerFragment.kt
@@ -0,0 +1,29 @@
+package com.example.crudrealtimeadmin
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+
+class DatePickerFragment(val listener: (dia:Int, mes:Int, anion:Int) -> Unit): DialogFragment (),
+    DatePickerDialog.OnDateSetListener {
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        listener(dayOfMonth, month, year)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c: Calendar = Calendar.getInstance()
+        val dia = c.get(Calendar.DAY_OF_MONTH)
+        val mes = c.get(Calendar.MONTH)
+        val anion = c.get(Calendar.YEAR)
+
+        val picker = DatePickerDialog(activity as Context, this, anion, mes, dia)
+        return picker
+
+    }
+
+}
