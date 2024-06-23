32182eba16ccaa33bade12d8c46470b46723cf7e
Steph <stephaniemelisav@gmail.com>
2024-06-23 02:38:22 -0300

    se añade generador de id

--- app/src/main/java/com/example/prueba_kt_bd/Usuario.kt
+++
@@ -1,7 +0,0 @@
-package com.example.prueba_kt_bd
-
-data class Usuario(
-    var id: Int? = null,
-    var nombre: String = "",
-    var edad: Int = 0
-)
--- crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/UploadActivity.kt
+++ crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/UploadActivity.kt
@@ -11,69 +11,53 @@ import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
-
 import com.example.crudrealtimeadmin.databinding.ActivityUploadBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import java.util.Calendar
 
-
-//aca vamos a conectar una bd con los datos creados
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
-    //referencia de la bd
     private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_upload)
-        //establecemos la conexión
+
         binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Se sobreescribe la función del botón atras del sistema.
+        databaseReference = FirebaseDatabase.getInstance().getReference("evento")
+
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 val intent = Intent(this@UploadActivity, MainActivity::class.java)
                 startActivity(intent)
                 finish()
             }
-        }
-        )
+        })
 
-        //____________Menú spinner para Materia y tipos_____________
-        val materias : Spinner = binding.spMaterias
-        val tipos :Spinner = binding.spTipo
-        // ---------Harcodeo lista materias
-        val listaMaterias = arrayOf("Seleccione materia", "Materia 01","Materia 02","Materia 03","Materia 04", "Materia 05")
+        val materias: Spinner = binding.spMaterias
+        val tipos: Spinner = binding.spTipo
+
+        val listaMaterias = arrayOf("Seleccione materia", "Materia 01", "Materia 02", "Materia 03", "Materia 04", "Materia 05")
         val adaptMaterias = ArrayAdapter(this, R.layout.item_spinner, listaMaterias)
-        // ---------Hardcodeo lista tipos
-        val listaTipos = arrayOf("Seleccione tipo" ,"Examen","Trabajo práctico","Otro")
-        val adaptTipos = ArrayAdapter(this, R.layout.item_spinner,listaTipos)
+        val listaTipos = arrayOf("Seleccione tipo", "Examen", "Trabajo práctico", "Otro")
+        val adaptTipos = ArrayAdapter(this, R.layout.item_spinner, listaTipos)
 
         materias.adapter = adaptMaterias
         tipos.adapter = adaptTipos
-        // ____________________Fin Spinners______________________
 
-        // Selector de Fecha
-        binding.etFecha.setOnClickListener{mostrarSelectorFecha()}
+        binding.etFecha.setOnClickListener { mostrarSelectorFecha() }
 
-
-        //Configurar TimePickerDialog para la hora, si clickea abre popup
         binding.etHora.setOnClickListener {
-            //en val calendar guardamos una instancia de calendar, que se usa para tener la hora y minutos
             val calendar = Calendar.getInstance()
-            //formato de 24hs, 0 a 23
             val hour = calendar.get(Calendar.HOUR_OF_DAY)
-            //minutos
             val minute = calendar.get(Calendar.MINUTE)
 
-            //Aca permitimos la selección de la hora al usuario (profesor)
             val timePickerDialog = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
-                //selectedHour, selectedMinute representan los minutos y hora seleccionada
                 binding.etHora.setText(String.format("%02d:%02d", selectedHour, selectedMinute))
             }, hour, minute, true)
 
@@ -81,44 +65,40 @@ class UploadActivity : AppCompatActivity() {
         }
 
         binding.saveButton.setOnClickListener {
-            // Elección de spinner
-            // Se valida valor de posición
-            val materiaSeleccionada : String? = if(materias.selectedItemPosition == 0) null else materias.selectedItem.toString()
-            val tipoSeleccionado : String? = if(tipos.selectedItemPosition == 0)null else tipos.selectedItem.toString()
-            //variables que guardan los datos
+            val materiaSeleccionada: String? = if (materias.selectedItemPosition == 0) null else materias.selectedItem.toString()
+            val tipoSeleccionado: String? = if (tipos.selectedItemPosition == 0) null else tipos.selectedItem.toString()
             val fecha = binding.etFecha.text.toString()
             val hora = binding.etHora.text.toString()
             val nombreExamen = binding.etNameExamen.text.toString()
 
+            val evento = DatoFecha(materiaSeleccionada, fecha, hora, nombreExamen, tipoSeleccionado)
 
-            databaseReference = FirebaseDatabase.getInstance().getReference("evento")
+            val uniqueKey = databaseReference.push().key
 
-            val evento = DatoFecha( materiaSeleccionada, fecha, hora, nombreExamen, tipoSeleccionado)
+            if (uniqueKey != null) {
+                databaseReference.child(uniqueKey).setValue(evento).addOnSuccessListener {
+                    binding.etFecha.text.clear()
+                    binding.etHora.text.clear()
+                    binding.etNameExamen.text.clear()
 
-            databaseReference.child(fecha).setValue(evento).addOnSuccessListener {
-                binding.etFecha.text.clear()
-                binding.etHora.text.clear()
-                binding.etNameExamen.text.clear()
-
-                // Muestra un pequeño aviso de que se subió correctamente
-                Toast.makeText(this, "Evento guardado.", Toast.LENGTH_LONG).show()
-            }.addOnFailureListener { e ->
-                // Mensaje en caso de falla
-                Toast.makeText(this, "Operación fallida: ${e.message}", Toast.LENGTH_LONG).show()
-                Log.e("UploadActivity", "Error al guardar el evento", e)
+                    Toast.makeText(this, "Evento guardado.", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener { e ->
+                    Toast.makeText(this, "Operación fallida: ${e.message}", Toast.LENGTH_LONG).show()
+                    Log.e("UploadActivity", "Error al guardar el evento", e)
+                }
+            } else {
+                Toast.makeText(this, "Error al generar ID único para el evento", Toast.LENGTH_LONG).show()
             }
-
         }
     }
 
     private fun mostrarSelectorFecha() {
-        val datePicker = DatePickerFragment{dia, mes, anion -> enLaFechaElegida(dia, mes, anion)}
-        datePicker.show(supportFragmentManager,"Selector fecha")
+        val datePicker = DatePickerFragment { dia, mes, anio -> enLaFechaElegida(dia, mes, anio) }
+        datePicker.show(supportFragmentManager, "Selector fecha")
     }
 
     @SuppressLint("SetTextI18n")
-    private fun enLaFechaElegida(dia:Int, mes:Int, anion:Int){
-        binding.etFecha.setText("$dia-$mes-$anion")
+    private fun enLaFechaElegida(dia: Int, mes: Int, anio: Int) {
+        binding.etFecha.setText("$dia-$mes-$anio")
     }
-
 }
