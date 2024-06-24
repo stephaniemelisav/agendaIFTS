41facc53029eb489b2c75d0dff512833d5abc157
ozunacristian <ozunacristianj@gmail.com>
2024-06-24 04:25:56 -0300

    Se hacen seleccionables los items en event view holder

--- crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/activitys/EventDetail.kt
+++ crudrealtimeadmin/src/main/java/com/example/crudrealtimeadmin/activitys/EventDetail.kt
@@ -1,6 +1,8 @@
 package com.example.crudrealtimeadmin.activitys
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -8,7 +10,14 @@ import com.example.crudrealtimeadmin.R
 
 class EventDetail : AppCompatActivity() {
 
-    private lateinit var
+    private lateinit var tvEventID: TextView
+    private lateinit var tvEventMateria: TextView
+    private lateinit var tvEventFecha: TextView
+    private lateinit var tvEventHora: TextView
+    private lateinit var tvEventDescript: TextView
+    private lateinit var btnActualizar: Button
+    private lateinit var btnEliminar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_detail)
